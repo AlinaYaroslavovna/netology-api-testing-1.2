7293907d6617bfb10f11c385d9dfdd542910484b
unknown <alina.yaroslavovna@gmail.com>
2025-09-28 23:05:26 +0300

    src/

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -2,7 +2,7 @@ package ru.netology.rest;
 
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
@@ -10,20 +10,14 @@ class MobileBankApiTestV3 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
                 // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
-                // Проверки
+                // код теста
                 .then()
                 .statusCode(200)
-                // специализированные проверки - лучше
-                .contentType(ContentType.JSON)
-                .body("", hasSize(3))
-                .body("[0].currency", equalTo("RUB"))
-                .body("[0].balance", greaterThanOrEqualTo(0))
-        ;
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
